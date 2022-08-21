ec4649952ea6489aa07346773bab0fd9bb604bf2
Yordan Galabov <galabovj@gmail.com>
2022-08-21 22:01:37 +0300

    code splitting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::run;
+use minigrep::Config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -17,29 +18,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         process::exit(1);
     }
     Ok(())
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
